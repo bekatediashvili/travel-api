bdfad8258a0762d1a5b8317ee362a5c076b75ce3
tatas <bekatediashvili1995@gmail.com>
2023-07-13 00:36:28 +0400

    Public Tour  Endpoint

---
+++ app/Http/Requests/ToursListRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ToursListRequest extends FormRequest
+{
+
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'priceFrom' => 'numeric',
+            'priceTo' => 'numeric',
+            'dateFrom' => 'date',
+            'dateTo' => 'date',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+
+            'priceFrom' => 'priceFrom is not Correct',
+            'dateFrom' => 'Date is not Valid'
+        ];
+    }
+}
