5133f4322033c0eb74767181075eb44a7e1a429c
tatas <bekatediashvili1995@gmail.com>
2023-07-12 22:49:49 +0400

    public travel Endpoint

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('travels', [\App\Http\Controllers\Api\V1\TravelController::class,'index']);
